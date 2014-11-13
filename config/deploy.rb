50bfc716a46f4252a606270a9a96cb47eba78d8e
jaspreet <jaspreet21anand@gmail.com>
2014-11-13 20:16:08 +0530

    add secrets file

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :branch, "server_setup"
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
