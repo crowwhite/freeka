f8d2ea9ed1c087015ebada5756ad206af78d4888
jaspreet <jaspreet21anand@gmail.com>
2014-11-18 17:41:36 +0530

    duplicate validation removal + mandatory fields

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,8 +2,7 @@ class Person < ActiveRecord::Base
 
   TYPES = %w(Admin User)
 
-  validates :name, :email, :contact_no, :about_me, :address, presence: true
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :name, :contact_no, presence: true
   validates :contact_no, numericality: true
   validates :type, inclusion: { in: TYPES, message: "%{ value } is not a valid type" }
   validate :no_spaces_in_password
