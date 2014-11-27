8bc60913fa4803802d3b20fbda513d91ae49b517
jaspreet <jaspreet21anand@gmail.com>
2014-11-27 17:59:55 +0530

    query optimization

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,8 @@ class WelcomeController < ApplicationController
     if current_admin
       redirect_to admins_welcome_index_path
     else
-      @requirements = Requirement.enabled.with_status_not(2).order(:expiration_date).page params[:page]
+      @requirements = Requirement.enabled.with_status_not(2).includes(:address, :donor_requirements, :categories, :interested_donors).order(:expiration_date).page params[:page]
+      load_donations
       render 'requirements/index'
     end
   end
