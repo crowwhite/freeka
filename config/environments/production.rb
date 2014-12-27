f5f5496557c1d6b4685157eb981545cf0f71556c
jaspreet <jaspreet21anand@gmail.com>
2014-12-27 13:57:11 +0530

    mailer views and requirement index page on admin end

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     domain: "domain.of.sender.net",
     authentication: "plain",
     user_name: "mail2freeka@gmail.com",
-    password: "mm22ffmm22ff",
+    password: "jaspreet@vinsol",
     enable_starttls_auto: true
   }
 
