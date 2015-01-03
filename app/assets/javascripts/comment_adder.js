04b9f5e93da5e26d56c59bb5c515db03cb8eea25
jaspreet <jaspreet21anand@gmail.com>
2015-01-03 11:53:26 +0530

    CR fixes - use dynamic key passing to js file + respond to both html and js in comment controller + use jquery syntax to add comment elements

--- app/assets/javascripts/comment_adder.js
+++ app/assets/javascripts/comment_adder.js
@@ -1,19 +1,15 @@
 var commentAdder = function(data) {
   $('.text-area').val('')
-  var comment = "<li class='highlight-grey-border round-corners'><strong>&nbsp;<a href='/users/'" +
-    data.user_id +
-    ">" +
-    data.user_name +
-    "</a></strong> commented on " +
-    data.time +
-    "<pre class='highlight-grey-border'>" +
-    data.comment +
-    "</pre></li>";
-  $('#add-comment').before(comment);
+  var $linkToUser = $('<a>', { href: '/users/' + data.user_id, html: data.user_name });
+  var $bold = $('<strong>', { html: '&nbsp;' });
+  var $content = $('<pre>', { html: data.comment, class: 'highlight-grey-border' });
+  var $listItem = $('<li>', { class: 'highlight-grey-border round-corners' });
+  var $comment = $listItem.html(' commented on ' + data.time).prepend($bold.append($linkToUser)).append($content);
+  $('#add-comment').before($comment);
 }
 
 $(function() {
-  var pusher = new Pusher('0ce2348f1428f9764747');
+  var pusher = new Pusher($('#app_key').val());
   var url = document.URL;
   var channel = pusher.subscribe('channel_' + url.substring(url.lastIndexOf('/') + 1));
   channel.bind('new_comment', function(data) {
