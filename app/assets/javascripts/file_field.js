1545a46732529d1413e0e15e4712d095df849973
jaspreet <jaspreet21anand@gmail.com>
2015-01-09 21:03:37 +0530

    fix multiple file upload

--- app/assets/javascripts/file_field.js
+++ app/assets/javascripts/file_field.js
@@ -3,7 +3,7 @@ function FileField(elementIdToBeAppendedIn) {
 }
 
 FileField.prototype.createFileFieldHtml = function() {
-  var uniqueString = new Date().toString();
+  var uniqueString = $.now();
   return "<br><br><div class='col-md-10'><label class='col-md-2 control-label' for='file_attachment'>File</label><div class='col-md-10'><div class='row'><div class='col-md-3'><div class='MultiFile-wrap' id='requirement_files_attributes_0_file_attachment_wrap'><input class='form-control multi MultiFile-applied' id='requirement_files_attributes_0_file_attachment' name='requirement[files_attributes][" + uniqueString + "][attachment]' type='file' value=''><div class='MultiFile-list' id='requirement_files_attributes_0_file_attachment_wrap_list'></div></div></div><!-- Caption --><label class='col-md-2 control-label' for='requirement_files_attributes_0_caption'>Caption</label><div class='col-md-6'><input class='form-control' id='requirement_files_attributes_0_caption' name='requirement[files_attributes][" + uniqueString + "][caption]' type='text' value=''></div></div></div></div>";
 }
 
